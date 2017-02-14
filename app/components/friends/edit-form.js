ab998df9a87753e02eb5791e23a47091823fa1e9
jeremy.arimado <jeremy.arimado@jaxsta.com>
2017-02-15 10:19:45 +1100

    failed to delete friend + adding styles

--- app/components/friends/edit-form.js
+++ app/components/friends/edit-form.js
@@ -17,7 +17,7 @@ export default Ember.Component.extend({
     },
     cancel() {
       console.log('+- cancel action in edit-form component');
-      this.cancel();
+      this.cancel(this.get('model'));
     }
   },
   isValid: computed(
--- app/models/friend.js
+++ app/models/friend.js
@@ -1,8 +1,15 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+const { computed } = Ember;
 
 export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   email: DS.attr('string'),
-  twitter: DS.attr('string')
+  twitter: DS.attr('string'),
+  fullName: computed('firstName', 'lastName', {
+    get() {
+      return this.get('firstName') + ' ' this.get('lastName');
+    }
+  })
 });
