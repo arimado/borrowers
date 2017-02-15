e38f1b10f60bb5aef74762533c57e16234f81ca4
jeremy.arimado <jeremy.arimado@jaxsta.com>
2017-02-15 12:31:24 +1100

    added loans as a nested route to friends

--- app/models/friend.js
+++ app/models/friend.js
@@ -1,12 +1,15 @@
 import DS from 'ember-data';
 import Ember from 'ember';
+import { hasMany } from 'ember-data/relationships';
 const { computed } = Ember;
+
 
 export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   email: DS.attr('string'),
   twitter: DS.attr('string'),
+  loans: hasMany('loan'),
   fullName: computed('firstName', 'lastName', {
     get() {
       return this.get('firstName') + ' ' + this.get('lastName');
