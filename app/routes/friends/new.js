4a5f5cee7277db5af549a422078d21d1091765c1
Jeremy Arimado <jarimado@Jeremys-MacBook-Air.local>
2017-02-15 20:05:09 +1100

    about to add javascript plugins

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -3,5 +3,20 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.createRecord('friend');
+  },
+  activate() {
+
+  },
+  deactivate() {
+
+  },
+  resetController: function (controller, isExiting) {
+    if (isExiting) {
+      console.log('----- resetController hook called -----');
+      const model = controller.get('model');
+      if ( model.get('isNew') ) {
+        model.destroyRecord();
+      }
+    }
   }
 });
